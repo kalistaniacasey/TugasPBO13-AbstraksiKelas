73550e44b5ff077a3b9f305fa27b69d4ad33627f
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-11-28 07:46:10 +0700

    Create Tumbuhan.java

---
+++ Tumbuhan.java
@@ -0,0 +1,11 @@
+public class Tumbuhan extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Tumbuhan bernapas dengan menyerap oksigen melalui stomata di daun.");
+    }
+
+    @Override
+    public void berkembangBiak() {
+        System.out.println("Tumbuhan berkembang biak dengan biji, tunas.");
+    }
+}

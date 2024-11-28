b79c1188a1b2704808216f370b8532ec750d9951
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-11-28 07:45:28 +0700

    Create Hewan.java

---
+++ Hewan.java
@@ -0,0 +1,11 @@
+public class Hewan extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Hewan bernapas menggunakan paru-paru, insang, atau permukaan tubuh.");
+    }
+
+    @Override
+    public void berkembangBiak() {
+        System.out.println("Hewan berkembang biak dengan bertelur/melahirkan.");
+    }
+}

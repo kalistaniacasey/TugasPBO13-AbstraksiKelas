c627a8dc1f70720c49b2fb1aca37439ddb9b6749
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-11-28 07:44:57 +0700

    Create Manusia.java

---
+++ Manusia.java
@@ -0,0 +1,11 @@
+public class Manusia extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Manusia bernapas dengan paru-paru, menghirup oksigen, dan mengeluarkan karbon dioksida.");
+    }
+
+    @Override
+    public void berkembangBiak() {
+        System.out.println("Manusia makan menggunakan alat makan seperti sendok atau tangan.");
+    }
+}

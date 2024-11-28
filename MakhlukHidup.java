923ca492a443cc84d86b3ac57f5c3e06f7cf2a6e
kalistaniacasey <162282241+kalistaniacasey@users.noreply.github.com>
2024-11-28 07:44:16 +0700

    Create MakhlukHidup.java

---
+++ MakhlukHidup.java
@@ -0,0 +1,8 @@
+public abstract class MakhlukHidup {
+    public abstract void bernapas();
+    public abstract void berkembangBiak();
+
+    public void makan() {
+        System.out.println("Makhluk hidup membutuhkan makanan untuk bertahan hidup.");
+    }
+}
